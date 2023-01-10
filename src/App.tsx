3eb654c0a1ae9bef5c93bb53ce21cad1c01366cf
vitor <vaguiarms@gmail.com>
2023-01-10 18:07:13 -0300

    Alter display weather

--- src/App.tsx
+++ src/App.tsx
@@ -61,17 +61,7 @@ function App() {
       style={{ backgroundImage: `url(${backgroundWeather})` }}
       className="w-full h-[100vh] py-20 bg-cover"
     >
-      <div>
-        <p className="px-10 font-sans font-nunito text-4xl	text-white">
-          {data.name}
-        </p>
-      </div>
-
-      <div className="mt-4">
-        <p className="px-10 text-8xl font-bold text-white	">
-          {data.main.temp}º F
-        </p>
-      </div>
+      
 
       <div className="flex justify-center">
         <input
@@ -85,23 +75,36 @@ function App() {
       </div>
 
       {/* Other stats */}
-      <div className="flex justify-center max-w-md mx-auto mt-36 bg-slate-900 bg-opacity-60 rounded-xl py-20">
+      <div className="flex justify-center max-w-6xl mx-auto mt-20 bg-slate-900 bg-opacity-60 rounded-xl py-20">
+        <div className="flex justify-center flex-col mr-10">
+          <p className="px-10 font-sans font-nunito text-4xl	text-white mb-4">
+            {data.name}
+          </p>
+          <p className="px-10 text-8xl font-bold text-white	">
+            {data.main.temp}º F
+          </p>
+          {/* <p className="text-2xl text-white">teste</p>
+          <p className="text-2xl text-white">teste</p> */}
+        </div>
         <div className="text-white font-semibold">
-          <div>
+          <div className="ml-10">
             {/* {data.weather[0].icon !== '' && <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="icon-weather" />}
              */}
             {/* <p className="flex justify-end text-4xl">
                {data.main.feels_like}
              </p> */}
-            <img className="" src={Icons(data.weather[0].main)} alt="icon-weather" />
-            <p className="text-2xl mt-4">Feels Like</p>
+            <p className="flex justify-center text-2xl mt-4">Feels Like</p>
+            <img
+              className=""
+              src={Icons(data.weather[0].main)}
+              alt="icon-weather"
+            />
 
-            <div className="grid grid-cols-3 divide-x-[3px] gap-2 mt-10 border rounded-lg p-2">
-              <div className="">01</div>
-              <div>02</div>
-              <div>03</div>
+            <div className="grid grid-cols-3 gap-2 mt-10 border rounded-lg p-2">
+              <div className="">teste 01</div>
+              <div className="">teste 02</div>
+              <div className="">teste 03</div>
             </div>
-            
           </div>
         </div>
       </div>
