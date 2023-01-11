e8cf4cfc6ebcd183689f9ee36c7bed0b2a9650e5
vitor <vaguiarms@gmail.com>
2023-01-11 19:32:56 -0300

    Display adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -67,8 +67,13 @@ function App() {
       </div>
 
       {/* Other stats */}
+      
       <div className="flex justify-center max-w-6xl mx-auto mt-20 bg-slate-900 bg-opacity-60 rounded-xl py-20">
+
+
+        {/* Fazer if ternario para exibição das infos */}
         <div className="flex justify-center flex-col mr-10">
+          <p className="px-10 text-3xl font-bold text-white	mb-4">{data.name}</p>
           <p className="px-10 text-8xl font-bold text-white	mb-4">
             {data.main.temp}º F
           </p>
@@ -84,24 +89,28 @@ function App() {
             </div>
           )}
         </div>
-        <div className="text-white font-semibold">
-          <div className="ml-10">
-            {/* {data.weather[0].icon !== '' && <img src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="icon-weather" />}
-             */}
-            {/* <p className="flex justify-end text-4xl">
-               {data.main.feels_like}
-             </p> */}
-            <img src={Icons(data.weather[0].main)} alt="icon-weather" />
 
-            <div className="grid grid-cols-2 gap-2 mt-10 border rounded-lg p-2">
-              <div className="">
-                <p className="text-lg">Humidity: {data.main.humidity}</p>
-              </div>
-              <div className="">
-                <p className="text-lg">Wind Speed: {data.wind.speed}</p>
-              </div>
-            </div>
+
+
+        <div className="text-white font-semibold">
+
+          <div className="ml-10">
+            {data.name && (
+              <>
+                <img src={Icons(data.weather[0].main)} alt="icon-weather" />
+
+                <div className="grid grid-cols-2 gap-2 mt-10 border rounded-lg p-2">
+                  <div className="">
+                    <p className="text-lg">Humidity: {data.main.humidity}</p>
+                  </div>
+                  <div className="">
+                    <p className="text-lg">Wind Speed: {data.wind.speed}</p>
+                  </div>
+                </div>
+              </>
+            )}
           </div>
+
         </div>
       </div>
     </div>
