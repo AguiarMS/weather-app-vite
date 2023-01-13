b6e274b16f33da9fb29b0f355c03296e078dd243
vitor <vaguiarms@gmail.com>
2023-01-13 18:28:41 -0300

    create searchButtonLocation

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,13 @@ function App() {
     }
   };
 
+  const searchButtonLocation = async () => {
+    await axios.get(url).then((response) => {
+      setData(response.data);
+      console.log("BUTTON SEARCH EVENT", response.data);
+    });
+  };
+
   const handleTime = () => {
     const timeStamp = data.dt || 0;
     const convertData = new Date(timeStamp * 1000).toLocaleString("pt-BR");
@@ -35,16 +42,21 @@ function App() {
       style={{ backgroundImage: `url(${backgroundWeather})` }}
       className="w-full h-[100vh]  bg-cover"
     >
-      
       <div className="flex justify-center">
         <input
-          className="mt-20 rounded py-2 px-2 w-[58%]"
+          className="mt-20 rounded-l-md py-2 px-2 w-[55%]"
           value={location}
           onChange={(event) => setLocation(event.target.value)}
           onKeyUpCapture={searchLocation}
           placeholder="Enter Location"
           type="text"
         />
+        <button
+          className="bg-slate-700 hover:bg-slate-600 mt-20 text-white font-bold py-2 px-4 rounded-r-md"
+          onClick={searchButtonLocation}
+        >
+          Search
+        </button>
       </div>
 
       {/* Other stats */}
