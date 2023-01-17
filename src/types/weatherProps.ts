8e12d1a6144dbf2c22b4b10340b00100d8846ece
vitor <vaguiarms@gmail.com>
2023-01-17 10:04:47 -0300

    commit

--- src/types/weatherProps.ts
+++ src/types/weatherProps.ts
@@ -1,5 +1,28 @@
 export type WeatherProps = {
   name: string;
-
+  main: {
+    feels_like: number;
+    humidity: number;
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+  };
+  wind: {
+    speed: number;
+  };
+  weather: [
+    {
+      icon: string;
+      main:
+        | "Thunderstorm"
+        | "Drizzle"
+        | "Rain"
+        | "Snow"
+        | "Clouds"
+        | "Haze"
+        | "Smoke"
+        | "Clear"
+    }
+  ];
   dt?: number;
 };
