dd0dedf3f39bb4ea038fcfaaf8dbcb7489881ae9
vitor <vaguiarms@gmail.com>
2023-01-18 00:10:29 -0300

    Simple alter

--- src/components/RandomLocation/index.tsx
+++ src/components/RandomLocation/index.tsx
@@ -1,7 +1,7 @@
 
 export function RandomLocation(data: string[]) {
   return (
-    <div className="flex justify-center max-w-6xl mx-auto mt-14 bg-slate-900 bg-opacity-60 rounded-xl py-20">
+    <div className="flex justify-center max-w-6xl mx-auto mt-14 bg-slate-900 bg-opacity-60 rounded-xl py-20 px-20">
       <div className="flex justify-center flex-col mr-10">
         <div className="flex space-x-4">
           {data.map((item) => (
