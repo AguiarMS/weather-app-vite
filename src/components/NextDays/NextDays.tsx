b4495a65883dc734aa4b7d1471c58e846b2bc14a
vitor <vaguiarms@gmail.com>
2023-01-25 21:13:43 -0300

    Function NextDays exclusion

--- src/components/NextDays/NextDays.tsx
+++ src/components/NextDays/NextDays.tsx
@@ -62,16 +62,4 @@ export const NextDaysProps = (dataDays: getLocationFor5daysProps[]) => {
   }
 };
 
-export const NextDays = () => {
-  return (
-    <div className="flex justify-center max-w-6xl mx-auto mt-12 bg-slate-900 bg-opacity-60 text-white rounded-xl py-10">
-      <div className="grid grid-cols-5 space-x-8">
-        {weekdays.map((weekday) => (
-          <div key={weekday.id}>
-            <p>{weekday.title}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
+
